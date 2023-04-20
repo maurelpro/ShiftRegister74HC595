ec698138af3a9a9a29c0e256aaa76c4683902cf8
maurelpro <120382376+maurelpro@users.noreply.github.com>
2023-04-20 16:28:36 +0100

    Create REG_74HC595.h
    This file helps for to command and identify easly pin of register who is connect to your OUTPUT.  See example Simple_CMD for more comprehension

---
+++ src/REG_74HC595.h
@@ -0,0 +1,135 @@
+#ifndef REG_74HC595
+#define REG_74HC595
+
+/* 
+
+    This header file helps you to command and identify easly 
+    pin of register who is connect to your OUTPUT. You can use
+    also name in your code
+
+    Just ask, What is connected to My output(led , CMD ...) ? 
+
+*/
+
+
+
+// REG 1
+#define REG_1_PIN_0		0
+#define REG_1_PIN_1		1
+#define REG_1_PIN_2		2
+#define REG_1_PIN_3		3
+#define REG_1_PIN_4		4
+#define REG_1_PIN_5		5
+#define REG_1_PIN_6		6
+#define REG_1_PIN_7		7
+
+
+
+// REG 2
+#define REG_2_PIN_0		8
+#define REG_2_PIN_1		9
+#define REG_2_PIN_2		10
+#define REG_2_PIN_3		11
+#define REG_2_PIN_4		12
+#define REG_2_PIN_5		13
+#define REG_2_PIN_6		14
+#define REG_2_PIN_7		15
+
+
+
+// REG 3
+#define REG_3_PIN_0		16
+#define REG_3_PIN_1		17
+#define REG_3_PIN_2		18
+#define REG_3_PIN_3		19
+#define REG_3_PIN_4		20
+#define REG_3_PIN_5		21
+#define REG_3_PIN_6		22
+#define REG_3_PIN_7		23
+
+
+
+// REG 4
+#define REG_4_PIN_0		24
+#define REG_4_PIN_1		25
+#define REG_4_PIN_2		26
+#define REG_4_PIN_3		27
+#define REG_4_PIN_4		28
+#define REG_4_PIN_5		29
+#define REG_4_PIN_6		30
+#define REG_4_PIN_7		31
+
+
+
+// REG 5
+#define REG_5_PIN_0		32
+#define REG_5_PIN_1		33
+#define REG_5_PIN_2		34
+#define REG_5_PIN_3		35
+#define REG_5_PIN_4		36
+#define REG_5_PIN_5		37
+#define REG_5_PIN_6		38
+#define REG_5_PIN_7		39
+
+
+
+// REG 6
+#define REG_6_PIN_0		40
+#define REG_6_PIN_1		41
+#define REG_6_PIN_2		42
+#define REG_6_PIN_3		43
+#define REG_6_PIN_4		44
+#define REG_6_PIN_5		45
+#define REG_6_PIN_6		46
+#define REG_6_PIN_7		47
+
+
+
+// REG 7
+#define REG_7_PIN_0		48
+#define REG_7_PIN_1		49
+#define REG_7_PIN_2		50
+#define REG_7_PIN_3		51
+#define REG_7_PIN_4		52
+#define REG_7_PIN_5		53
+#define REG_7_PIN_6		54
+#define REG_7_PIN_7		55
+
+
+
+// REG 8
+#define REG_8_PIN_0		56
+#define REG_8_PIN_1		57
+#define REG_8_PIN_2		58
+#define REG_8_PIN_3		59
+#define REG_8_PIN_4		60
+#define REG_8_PIN_5		61
+#define REG_8_PIN_6		62
+#define REG_8_PIN_7		63
+
+
+
+// REG 9
+#define REG_9_PIN_0		64
+#define REG_9_PIN_1		65
+#define REG_9_PIN_2		66
+#define REG_9_PIN_3		67
+#define REG_9_PIN_4		68
+#define REG_9_PIN_5		69
+#define REG_9_PIN_6		70
+#define REG_9_PIN_7		71
+
+
+
+// REG 10
+#define REG_10_PIN_0		72
+#define REG_10_PIN_1		73
+#define REG_10_PIN_2		74
+#define REG_10_PIN_3		75
+#define REG_10_PIN_4		76
+#define REG_10_PIN_5		77
+#define REG_10_PIN_6		78
+#define REG_10_PIN_7		79
+
+
+#endif
